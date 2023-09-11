38a93aba5eb95adc79050d79f417643314a40392
Rafael.Chaves <rafaelchaveslv@gmail.com>
2023-09-10 21:13:14 -0300

    estruturacao de pastas, componentes e views

---
+++ src/components/components-styled/Navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navbar = styled.nav`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 7vh;
+    border-radius: 0rem 0rem 0.25rem 0.25rem;
+    background: #0B99FF;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+export default Navbar;
